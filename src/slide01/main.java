d5031ab3669f5ae834fc45f8204f908920520fd0
Khoi Nguyen <khoinguynn2005@gmail.com>
2026-03-10 22:04:24 +0700

    first commit

---
+++ src/slide01/main.java
@@ -0,0 +1,15 @@
+package slide01;
+
+public class main {
+    public static void main (String[] args) {
+
+        Xe xe = new Xe();
+
+        xe.sethangXe("Toyota");
+        xe.setnamSanXuat(2025);
+
+        xe.xuat();
+
+        System.out.println("22 PS48331 Nguyen Truong Minh Khoi");
+    }
+}
